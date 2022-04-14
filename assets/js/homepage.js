3789d570f0bb3fe34a9d850f7a007f440b78d7a8
msnaye <99111936+msnaye@users.noreply.github.com>
2022-04-14 14:04:18 -0500

    added get user button

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,8 +5,9 @@ var getUserRepos = function(user){
   // make a request to the url
   fetch(apiUrl).then(function(response){
       response.json().then(function(data){
-        console.log(date);
+        console.log(data);
       });
   });
 };
 
+getUserRepos("msnaye");
