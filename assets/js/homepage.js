72f14014d1b76b192220f25f4ba15c6a86373177
msnaye <99111936+msnaye@users.noreply.github.com>
2022-04-17 19:49:53 -0500

    completed module

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl =document.querySelector("#user-form");
 var nameInputEl =document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl=document.querySelector("#language-buttons");
 
 var formSubmitHandler=function(event){
    // prevent page from refreshing
@@ -22,6 +23,18 @@ var formSubmitHandler=function(event){
  }  
 };
 
+var buttonClickHandler = function(event) {
+    //get the language attribute from the clicked element 
+    var language = event.target.getAttribute("data-language")
+    console.log(language)
+    if (language) {
+       getFeaturedRepos(language);
+     
+       // clear old content
+       repoContainerEl.textContent = "";
+     }
+};
+
 var getUserRepos = function(user){
   // format the githun api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -37,7 +50,7 @@ fetch(apiUrl)
               displayRepos(data, user);
           });
         } else{
-            alert("Error: GitHub User Not Found");
+            alert("Error: " + response.statusText);
         } 
     })
     .catch(function(error) {
@@ -45,6 +58,23 @@ fetch(apiUrl)
         alert("Unable to connect to GitHub");
       });
 };
+    var getFeaturedRepos = function(language) {
+        //format the github api URL
+      var apiUrl= "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    // make a get request to url 
+    fetch(apiUrl).then(function(response) {
+        //request was successful 
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }else{
+            alert("Error: " + response.statusText);
+        }
+        });
+    };
+
     var displayRepos = function(repos, searchTerm){
         // check if api returned any repos
     if (repos.length === 0) {
@@ -60,8 +90,9 @@ fetch(apiUrl)
     var repoName = repos[i].owner.login + "/" + repos[i].name;
   
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo="+repoName);
   
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
@@ -81,13 +112,18 @@ fetch(apiUrl)
 }   else {
     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
 }
-
+    
     // append to container
     repoEl.appendChild(statusEl);
 
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
-  }
-};
+
     // add event listeners to forms
     userFormEl.addEventListener("submit", formSubmitHandler);
+
+    // add event listener to clicked buttons    
+    languageButtonsEl.addEventListener("click", buttonClickHandler);
+
+}
+};
